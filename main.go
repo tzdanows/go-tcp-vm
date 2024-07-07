41057aafc0d478c602ad1fbc90cd69b45faa7df4
Tom Zdanowski <tzdanows@Toms-MBP.lan>
2024-07-07 13:08:18 -0700

    #1 finished basic tcp echo server in go

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+)
+
+func main() {
+	listener, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		fmt.Println("Error starting server: ", err)
+		os.Exit(1)
+	}
+	defer listener.Close()
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err)
+			continue
+		}
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+	scanner := bufio.NewScanner(conn)
+
+	for scanner.Scan() {
+		text := scanner.Text()
+		fmt.Println("Received from client: ", text)
+		conn.Write([]byte("Message received: " + text + "\n"))
+	}
+	fmt.Println("Error reading from connection: ", scanner.Err())
+}
